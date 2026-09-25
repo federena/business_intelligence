f49b544457b2c312fba35284df95012f3fa79111
federena <federena@miamioh.edu>
2026-09-24 20:14:58 -0400

    Added theme and tittle

--- apps/midwest_airbnb/app.R
+++ apps/midwest_airbnb/app.R
@@ -1,5 +1,7 @@
 
 library(querychat)
+library(shiny)
+library(bslib)
 
 con = DBI::dbConnect(RSQLite::SQLite(), "data/midwest_airbnb.db")
 
@@ -17,4 +19,31 @@ qc = querychat(
   extra_instructions = "data/extra_instructions.md"
 )
 
-qc$app_obj()
+
+ui = page_sidebar(
+  title   = "Midwest Airbnb Explorer",
+  theme   = bs_theme(primary = "#C3142D",
+                     base_font = font_google("Lato")),
+  sidebar = qc$sidebar(width = 350),
+  card(card_header(textOutput("title")),
+       DT::DTOutput("table")),
+  accordion(open = FALSE,
+            accordion_panel("SQL", verbatimTextOutput("sql")),
+            accordion_panel("About", "Airbnb Listings; built by Nathan Federer"))
+)
+
+server = function(input, output, session) {
+  vals = qc$server()
+  output$title = renderText(vals$title() %||% "All postings")
+  output$table = DT::renderDT(vals$df(),
+                              options = list(pageLength = 10))
+  output$sql   = renderText(vals$sql() %||%
+                              "SELECT * FROM listings")
+}
+
+shinyApp(ui, server)
+
+
+
+
+
